0bd7293302b76bd2c13edb049e8d80556a5d01b3
Nandini-49 <nandini.cse.21@nitap.ac.in>
2025-01-03 02:28:41 +0530

    maps all

--- src/components/InfoBox/components/MapsBox/MapsBox.tsx
+++ src/components/InfoBox/components/MapsBox/MapsBox.tsx
@@ -9,25 +9,69 @@ interface mapData {
 
 const maps: mapData[] = [
   {
-    imageUrl: "/src/assets/image.png",
+    imageUrl: "/src/assets/map1.png",
     imageTitle: "Conquest Large \n Dawn Breaker",
   },
-  { imageUrl: "/src/assets/game2.png", imageTitle: "Title 2" },
-  { imageUrl: "/src/assets/game3.png", imageTitle: "Title 3" },
-  { imageUrl: "/src/assets/game3.png", imageTitle: "Title 3" },
-  { imageUrl: "/src/assets/game3.png", imageTitle: "Title 3" },
-  { imageUrl: "/src/assets/game3.png", imageTitle: "Title 3" },
-  { imageUrl: "/src/assets/game3.png", imageTitle: "Title 3" },
-  { imageUrl: "/src/assets/game3.png", imageTitle: "Title 3" },
-  { imageUrl: "/src/assets/game3.png", imageTitle: "Title 3" },
-  { imageUrl: "/src/assets/game3.png", imageTitle: "Title 3" },
-  { imageUrl: "/src/assets/game3.png", imageTitle: "Title 3" },
-  { imageUrl: "/src/assets/game3.png", imageTitle: "Title 3" },
-  { imageUrl: "/src/assets/game3.png", imageTitle: "Title 3" },
-  { imageUrl: "/src/assets/game3.png", imageTitle: "Title 3" },
-  { imageUrl: "/src/assets/game3.png", imageTitle: "Title 3" },
-  { imageUrl: "/src/assets/game3.png", imageTitle: "Title 3" },
-  { imageUrl: "/src/assets/game3.png", imageTitle: "Title 3" },
+  {
+    imageUrl: "/src/assets/map2.png",
+    imageTitle: "Conquest Large \n Propoganda",
+  },
+  {
+    imageUrl: "/src/assets/map3.png",
+    imageTitle: "Conquest Large \n Operation Locker",
+  },
+  {
+    imageUrl: "/src/assets/map4.png",
+    imageTitle: "Conquest Large \n Lancang Dam",
+  },
+  {
+    imageUrl: "/src/assets/map5.png",
+    imageTitle: "Conquest Large \n Seige of Shanghai",
+  },
+  {
+    imageUrl: "/src/assets/map6.png",
+    imageTitle: "Conquest Large \n Golmud Railway",
+  },
+  {
+    imageUrl: "/src/assets/map7.png",
+    imageTitle: "Conquest Large \n Propoganda",
+  },
+  {
+    imageUrl: "/src/assets/map8.png",
+    imageTitle: "Conquest Large \n Seige of Shanghai",
+  },
+  {
+    imageUrl: "/src/assets/map9.png",
+    imageTitle: "Conquest Large \n DawnBreaker",
+  },
+  {
+    imageUrl: "/src/assets/map10.png",
+    imageTitle: "Conquest Large \n Propaganda",
+  },
+  {
+    imageUrl: "/src/assets/map11.png",
+    imageTitle: "Conquest Large \n Operation Locker",
+  },
+  {
+    imageUrl: "/src/assets/map12.png",
+    imageTitle: "Conquest Large \n Lancang Dam",
+  },
+  {
+    imageUrl: "/src/assets/map13.png",
+    imageTitle: "Conquest Large \n Seige of Shanghai",
+  },
+  {
+    imageUrl: "/src/assets/map14.png",
+    imageTitle: "Conquest Large \n Golmud Railway",
+  },
+  {
+    imageUrl: "/src/assets/map15.png",
+    imageTitle: "Conquest Large \n Propaganda",
+  },
+  {
+    imageUrl: "/src/assets/map16.png",
+    imageTitle: "Conquest Large \n Seige of Shanghai",
+  },
 ];
 
 const MapsBox: React.FC = () => {
@@ -35,7 +79,7 @@ const MapsBox: React.FC = () => {
     <>
       <h5>Map Rotation</h5>
       <div className={styles.container}>
-        {Array.from({ length: 10 }, (_, i) => i + 1).map(
+        {Array.from({ length: 16 }, (_, i) => i + 1).map(
           (__: number, index: Key) => {
             return (
               <MapCard
